cacb18618d1f7d9a7e0623bc6861b647a28c5827
ThanhsonWeb <sondeptroi@gmail.com>
2026-07-31 20:16:03 +0700

    Catching Error in Async function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,11 +1,12 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
+const catchAsync = require("../utils/catchAsync");
 
 // request Handlers
-exports.signup = async (req, res, next) => {
+exports.signup = catchAsync(async (req, res, next) => {
 	const newUser = await User.create(req.body);
-
 	res.status(201).json({
 		status: "success",
 		data: newUser,
 	});
-};
+});
