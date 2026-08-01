b2a86636cf2b50026021f66dd23f3e86e0e90685
ThanhsonWeb <sondeptroi@gmail.com>
2026-08-01 09:51:09 +0700

    UnhandleRejection and unCaughtExeption for error outside express

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 require("dotenv").config();
-const app = require("./app");
 const mongoose = require("mongoose");
+process.on("uncaughtException", (err) => {
+	console.error("UNCAUGHT EXCEPTION! 💥 Đang tắt server...");
+	console.error(err.name, err.message, err.stack);
+	// (unclean state -> exit right away ).
+	process.exit(1);
+});
+// 2. Load file app after uncaughtException's protection
+const app = require("./app");
 
-// connect to mongoDB
+//-----------------------Connect to mongoDB------------
 mongoose
 	.connect(process.env.DATABASE)
 	.then(() => console.log("MongoDB Connected!"));
-// start server
+//------------------------start server----------------------
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
+});
+
+//this listener function will run after any promise got error
+process.on("unhandledRejection", (err) => {
+	console.error(err.name, err.message, err.stack);
+	// stop connect receive req from users and shutdown !
+	server.close(() => {
+		process.exit(1);
+	});
 });
