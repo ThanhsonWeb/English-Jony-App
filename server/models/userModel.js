17dd5db2423a067e39acdd9c7b2236f9bde96670
ThanhsonWeb <sondeptroi@gmail.com>
2026-08-01 19:27:18 +0700

    User Model validation , passwordConfirm validation

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,16 +1,37 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userModel = new mongoose.Schema({
-	name: { type: String, required: [true, "user must have a name"] },
+	name: {
+		type: String,
+		required: [true, "user must have a name"],
+		trim: true,
+		minLength: [3, "name must have at least 3 characters"],
+		maxLength: [20, " maximum 20 characters"],
+	},
 	email: {
 		type: String,
 		required: [true, "user must have an email"],
 		unique: true,
+		lowercase: true,
+		validate: [validator.isEmail, "please provide a valid email"],
 	},
-	password: { type: String, required: [true, "user must have a password"] },
+	password: {
+		type: String,
+		required: [true, "user must have a password"],
+		minLength: [8, "Password must be at least 8 characters"],
+	},
 	passwordConfirm: {
 		type: String,
 		required: [true, "please confirm your password"],
+		// This validator only works on save() and create()
+		// because "this" refers to current document ! 
+		// so findByIdAndUpdate() ≠ current document -> validator doesn't work .
+		validate: {
+			validator: function (curValue) {
+				return curValue === this.password;
+			},
+			message: "Passwords are not the same",
+		},
 	},
 });
 
