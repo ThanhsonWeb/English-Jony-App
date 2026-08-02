7940fbc88bef94b8b237c675d09f3879becef865
ThanhsonWeb <sondeptroi@gmail.com>
2026-08-02 10:09:51 +0700

    generate JWT

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,10 +1,18 @@
 const User = require("../models/userModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
+const jwt = require("jsonwebtoken");
+
+const signToken = (id) => {
+	return jwt.sign({ id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN,
+	});
+};
 
 // request Handlers
 exports.signup = catchAsync(async (req, res, next) => {
 	const newUser = await User.create(req.body);
+
 	res.status(201).json({
 		status: "success",
 		data: newUser,
@@ -24,7 +32,12 @@ exports.login = catchAsync(async (req, res, next) => {
 	if (!user || !(await user.correctPassword(password, user.password))) {
 		return next(new AppError("Incorrect email or password!", 401));
 	}
+
+	const token = signToken(user._id);
+
 	res.status(200).json({
 		status: "success",
+		token,
+		data: { user },
 	});
 });
