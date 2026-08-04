7b8e38eb04ad7ace41738df2e04c760ce4066cfb
ThanhsonWeb <sondeptroi@gmail.com>
2026-08-04 16:26:41 +0700

    perform CRUD on Vocab in vocabController

--- server/controllers/vocabController.js
+++ server/controllers/vocabController.js
@@ -1,7 +1,9 @@
 const Vocab = require("../models/vocabModel");
 const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 exports.getAllVocab = catchAsync(async (req, res, next) => {
+	// Get current user's vocab
 	const vocabularies = await Vocab.find({ user: req.user.id });
 
 	res.status(200).json({
@@ -10,15 +12,49 @@ exports.getAllVocab = catchAsync(async (req, res, next) => {
 	});
 });
 
-exports.getAllVocab = catchAsync(async (req, res, next) => {
-	await Vocab.create({
-		user: req.user.id,
-		english: "apple",
-		vietnamese: "quả táo",
-	});
-
+exports.getVocab = catchAsync(async (req, res, next) => {
+	const vocab = await Vocab.findById(req.params.id);
+	if (!vocab) return next(new AppError("No vocabulary found", 404));
 	res.status(200).json({
 		status: " success",
-		data: { vocabularies },
+		data: { vocab },
+	});
+});
+exports.updateVocab = catchAsync(async (req, res, next) => {
+	const updatedVocab = await Vocab.findOneAndUpdate(
+		// This ensures users can only update their own vocabulary.
+		{ _id: req.params.id, user: req.user.id },
+		req.body,
+		{
+			new: true,
+			runValidators: true,
+		},
+	);
+	res.status(200).json({
+		status: " success",
+		data: { updatedVocab },
+	});
+});
+
+exports.createNewVocab = catchAsync(async (req, res, next) => {
+	const newVocab = await Vocab.create({
+		...req.body,
+		user: req.user.id,
+	});
+
+	res.status(201).json({
+		status: " success",
+		data: { newVocab },
+	});
+});
+
+exports.deleteVocab = catchAsync(async (req, res, next) => {
+	// only delete the logged-in user's vocabulary
+	await Vocab.findOneAndDelete({
+		_id: req.params.id,
+		user: req.user.id,
+	});
+	res.status(204).json({
+		status: "success",
 	});
 });
--- server/routes/vocabRoutes.js
+++ server/routes/vocabRoutes.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const { getAllVocab } = require("../controllers/vocabController");
+const {
+	getAllVocab,
+	createNewVocab,
+	getVocab,
+	updateVocab,
+	deleteVocab,
+} = require("../controllers/vocabController");
 const { protect } = require("../controllers/authController");
 
 const router = express.Router();
 
 // routes
-router.route("/").get(protect, getAllVocab);
-router.route("/").post(protect, getAllVocab);
+router.route("/").get(protect, getAllVocab).post(protect, createNewVocab);
+
+router
+	.route("/:id")
+	.get(protect, getVocab)
+	.patch(protect, updateVocab)
+	.delete(protect, deleteVocab);
 
 module.exports = router;
